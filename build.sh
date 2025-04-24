a47bf69c93ffd8705b7b713e58ff2fe11d25ce15
wafardev <155046671+wafardev@users.noreply.github.com>
2025-04-24 02:14:48 +0200

    removed build

--- build.sh
+++
@@ -1,7 +0,0 @@
-echo "🔧 Updating apt-get and installing ffmpeg..."
-sudo apt-get update && sudo apt-get install -y ffmpeg
-echo "✅ ffmpeg installed!"
-
-echo "📦 Installing Python dependencies..."
-pip install -r requirements.txt
-echo "✅ Dependencies installed!"
